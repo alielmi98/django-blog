593520b745f8e33c11d22b2609094c6f426c8929
Ali Elmi <ali.elmi77@yahoo.com>
2024-01-30 18:35:39 +0330

    create the app called website and Transfer the template to the project
    +create website app
    +transfer template to project
    +Connecting the template to the website app

---
+++ website/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from .views import index_view,about_view,contact_view,elements_view
+app_name='website'
+urlpatterns = [
+    path('',index_view,name='index'),
+    path('about',about_view,name='about'),
+    path('contact',contact_view,name='contact'),
+    path('elements',elements_view,name='elements'),
+]
