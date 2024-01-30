6b258813d30052a2d89aedc7b641c155bce9d963
Ali Elmi <ali.elmi77@yahoo.com>
2024-01-30 19:00:18 +0330

    Connected the template to the blog app

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-
+from blog.views import blog_index_view,blog_single_view
+app_name='blog'
 urlpatterns = [
-    
-
-    
+    path('',blog_index_view,name='index'),
+    path('single',blog_single_view,name='single'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,7 @@
 from django.shortcuts import render
+
+def blog_index_view(request):
+    return render(request,"blog/blog-home.html")
+
+def blog_single_view(request):
+    return render(request,"blog/blog-single.html")
