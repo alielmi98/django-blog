6cea61d8ebb728c544914c6bcce25df9f696e719
Ali Elmi <ali.elmi77@yahoo.com>
2024-02-17 22:41:18 +0330

    fixed bog in lastes post and category counter

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,16 +1,19 @@
 from django import template
 from blog.models import Post,Category
+from django.utils import timezone
 
 register=template.Library()
 
 @register.inclusion_tag("blog/blog_lastes_post.html")
 def lastespost(arg=3):
-    posts=Post.objects.filter(status=1).order_by('-published_date')[:arg]
+    now=timezone.now()
+    posts=Post.objects.filter(status=1).filter(published_date__lte = now).order_by('-published_date')[:arg]
     return {'posts':posts}
 
 @register.inclusion_tag("blog/blog_post_categories.html")
 def postcategories():
-    posts=Post.objects.filter(status=1)
+    now=timezone.now()
+    posts=Post.objects.filter(status=1).filter(published_date__lte = now)
     categories=Category.objects.all()
     cat_dic={}
     for name in categories:
--- website/templatetags/website_tags.py
+++ website/templatetags/website_tags.py
@@ -1,8 +1,10 @@
 from django import template
 from blog.models import Post,Category
+from django.utils import timezone
 
 register=template.Library()
 @register.inclusion_tag("website/website_lastest_post.html")
 def lastespost(arg=6):
-    posts=Post.objects.filter(status=1).order_by('-published_date')[:arg]
+    now=timezone.now()
+    posts=Post.objects.filter(status=1).filter(published_date__lte = now).order_by('-published_date')[:arg]
     return {'posts':posts}
